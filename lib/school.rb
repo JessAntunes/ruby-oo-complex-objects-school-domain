b5b79d653cc12eb7672b12574f5036a3b385f237
Jessica Antunes <jessantunes@protonmail.com>
2021-03-02 16:03:55 -0500

    commit tree e1bec0d665d14b76623c092ea2ddc7b84216d049

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+    attr_accessor :roster, :student, :grade_level, :name
+
+    def initialize(school_name, roster = {})
+        @school = school_name
+        @roster = roster
+    end
+
+    def add_student(student, grade_level) 
+        
+        @roster[grade_level] ||= []
+        @roster[grade_level]<<student
+             
+    end
+
+    def grade(grade_level)
+        @roster[grade_level]
+    end
+
+    def sort
+
+        sorted_students = {}
+    #sort only student array alphabetically
+        @roster.each do | grade, students |
+            sorted_students[grade] = students.sort
+        end
+        sorted_students
+
+    end
+
+end
+
+
